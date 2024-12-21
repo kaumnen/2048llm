63e43711161f5083c4391fc117a2cd214c33704e
kaumnen <dev@kaumnen.com>
2024-12-21 22:34:21 +0100

    refactor app py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-import uuid
-
-from playwright.async_api import async_playwright, Browser, Page
+from utils.core import initial_setup, current_state, next_move
+from utils.llm import start_llm
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="./2048"), name="static")
 
-active_games: dict[str, tuple[Browser, Page]] = {}
+app.mount("/static", StaticFiles(directory="./2048"), name="static")
 
 
 @app.get("/")
@@ -19,48 +17,23 @@ async def show_game():
 
 @app.get("/start-game")
 async def start_game():
-    playwright = await async_playwright().start()
-    chromium = playwright.chromium
-    browser = await chromium.launch(headless=False, args=["--start-maximized"])
-    page = await browser.new_page()
-    await page.set_viewport_size({"width": 800, "height": 1200})
-    await page.goto("http://localhost:8000")
-    await page.wait_for_load_state("networkidle")
-
-    await page.evaluate(
-        "var gameManager = new GameManager(4,KeyboardInputManager,HTMLActuator,LocalStorageManager);"
-    )
-
-    session_id = str(uuid.uuid4())
-    active_games[session_id] = (browser, page)
+    session_id = await initial_setup()
     return {"session_id": session_id}
 
 
 @app.post("/move")
 async def move(session_id: str, direction: int):
-    if session_id not in active_games:
-        raise HTTPException(
-            status_code=404, detail="Session not found. Start a game first."
-        )
-
-    _, page = active_games[session_id]
-
-    await page.evaluate(f"gameManager.move({direction})")
-
-    game_state = await page.evaluate("JSON.stringify(gameManager.serialize())")
-
-    return {"game_state": game_state}
+    new_state = await next_move(session_id, direction)
+    return {"game_state": new_state}
 
 
 @app.get("/get-state")
-async def get_scope(session_id: str):
-    if session_id not in active_games:
-        raise HTTPException(
-            status_code=404, detail="Session not found. Start a game first."
-        )
+async def get_scope(session_id: str, granularity: str = "full"):
+    game_state = await current_state(session_id)
 
-    _, page = active_games[session_id]
+    return {"game_state": game_state[granularity]}
 
-    game_state = await page.evaluate("JSON.stringify(gameManager.serialize())")
 
-    return {"game_state": game_state}
+@app.post("/start-llm")
+async def init_llm_solve(session_id: str):
+    await start_llm(session_id)
