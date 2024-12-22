fb11992302412fca3fdb2519000942832cc66fce
kaumnen <dev@kaumnen.com>
2024-12-22 20:25:49 +0100

    refactor main file

--- app.py
+++ app.py
@@ -1,39 +1,19 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-from utils.core import initial_setup, current_state, next_move
-from utils.llm import start_llm
+from routers.core import router as core_router
+from routers.llm import router as llm_router
+
 
 app = FastAPI()
-
 
 app.mount("/static", StaticFiles(directory="./2048"), name="static")
 
 
-@app.get("/")
+@app.get("/", tags=["default"])
 async def show_game():
     return FileResponse("./2048/index.html")
 
 
-@app.get("/start-game")
-async def start_game():
-    session_id = await initial_setup()
-    return {"session_id": session_id}
-
-
-@app.post("/move")
-async def move(session_id: str, direction: int):
-    new_state = await next_move(session_id, direction)
-    return {"game_state": new_state}
-
-
-@app.get("/get-state")
-async def get_scope(session_id: str, granularity: str = "full"):
-    game_state = await current_state(session_id)
-
-    return {"game_state": game_state[granularity]}
-
-
-@app.post("/start-llm")
-async def init_llm_solve(session_id: str):
-    await start_llm(session_id)
+app.include_router(core_router, prefix="/core", tags=["CORE"])
+app.include_router(llm_router, prefix="/llm", tags=["LLM"])
